1e5863ccacd6eb06493d251d280831fdc5f319c5
Anastasia <nastiyayesypenko@ukr.net>
2019-03-12 02:39:35 +0200

    style change

--- js/index.js
+++ js/index.js
@@ -2,12 +2,22 @@
 
 const leftButton = document.querySelector('.js-dictionary-pages-left');
 const rightButton = document.querySelector('.js-dictionary-pages-right');
+let dictionaryImage = document.querySelector('.dictionary-pages-image');
 
-function onLeftClick() {
-    console.log('left')
-};
-function onLeftClick() {
-    console.log('right');
-};
-leftButton.addEventListener('click', onLeftClick);
-rightButton.addEventListener('click', onRightClick);
+
+let dictionarySources = ['./img/page1.jpg', './img/page2.jpg', './img/page3.jpg', './img/page4.jpg', './img/page5.jpg'];
+document.addEventListener("DOMContentLoaded", onLoaded);
+function onLoaded() {
+    dictionaryImage.setAttribute('src', dictionarySources[0]);
+
+    function onLeftClick() {
+        dictionaryImage.removeAttribute('src');
+        dictionaryImage.setAttribute('src', dictionarySources[1]);
+    };
+    function onRightClick() {
+        dictionaryImage.removeAttribute('src');
+        dictionaryImage.setAttribute('src', './img/page2.jpg');
+    };
+    leftButton.addEventListener('click', onLeftClick);
+    rightButton.addEventListener('click', onRightClick);
+}
