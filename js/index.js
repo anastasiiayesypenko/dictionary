a41f401336da25c8b6690f61ec42963f95750fa0
Anastasia <nastiyayesypenko@ukr.net>
2019-03-13 00:34:38 +0200

    added slider

--- js/index.js
+++ js/index.js
@@ -6,17 +6,40 @@ let dictionaryImage = document.querySelector('.dictionary-pages-image');
 
 
 let dictionarySources = ['./img/page1.jpg', './img/page2.jpg', './img/page3.jpg', './img/page4.jpg', './img/page5.jpg'];
+dictionaryImage.setAttribute('src', dictionarySources[0]);
+
+
+
 document.addEventListener("DOMContentLoaded", onLoaded);
 function onLoaded() {
-    dictionaryImage.setAttribute('src', dictionarySources[0]);
-
+    let currentSource = dictionaryImage.getAttribute('src');
+    leftButton.disabled = true;
     function onLeftClick() {
+        rightButton.disabled = false;
+        currentSource = dictionaryImage.getAttribute('src');
+        let indexOfImage = dictionarySources.indexOf(currentSource);
+        console.log(indexOfImage);
         dictionaryImage.removeAttribute('src');
-        dictionaryImage.setAttribute('src', dictionarySources[1]);
+        if (indexOfImage >= 2) {
+            dictionaryImage.setAttribute('src', dictionarySources[indexOfImage-=1]) 
+        } else {
+            leftButton.disabled = true;
+            dictionaryImage.setAttribute('src', dictionarySources[0]);
+        };
     };
     function onRightClick() {
+        leftButton.disabled = false;
+        currentSource = dictionaryImage.getAttribute('src');
+        let indexOfImage = dictionarySources.indexOf(currentSource);
+        console.log(indexOfImage);
         dictionaryImage.removeAttribute('src');
-        dictionaryImage.setAttribute('src', dictionarySources[0]);
+        if (indexOfImage <= 2) {
+            dictionaryImage.setAttribute('src', dictionarySources[indexOfImage+=1]);
+        } else {
+            rightButton.disabled = true;
+            dictionaryImage.setAttribute('src', dictionarySources[4]);    
+        }
+
     };
     leftButton.addEventListener('click', onLeftClick);
     rightButton.addEventListener('click', onRightClick);
